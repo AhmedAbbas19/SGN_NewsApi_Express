20a1f6c459a00600c85a581090cab9e11f1ea4b8
AhmedAbbas19 <ahmeddabbas19@yahoo.com>
2020-06-17 18:37:41 +0200

    Cors edit

--- app.js
+++ app.js
@@ -6,11 +6,7 @@ const cors = require("cors");
 
 const app = express();
 
-var corsOptions = {
-  origin: "http://localhost:3000",
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use("/users", usersRouter);
 app.use("/news", newsRouter);
